3a77558ed75b721a7ab35cdf736cd51e0f570eab
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2024-01-21 12:07:40 -0500

    showing details to product

--- app/src/main/java/com/example/produra/presentation/productList/ProductListView.kt
+++ app/src/main/java/com/example/produra/presentation/productList/ProductListView.kt
@@ -2,7 +2,9 @@ package com.example.produra.presentation.productList
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
@@ -12,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.produra.presentation.productList.components.ProductListComponent
@@ -29,10 +32,21 @@ fun ProductListView(
     }
 
     Column {
-        Text(text = "Hello World", style = MaterialTheme.typography.titleMedium)
+        Text(
+            text = "Productos",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .padding(25.dp),
+            textAlign = TextAlign.Center
+        )
         ProductListComponent(
             products = state.products,
-            toggleProductMustBePurchased = { product -> viewModel.toggleProductMustBePurchased(product) }
+            toggleProductMustBePurchased = { product ->
+                viewModel.toggleProductMustBePurchased(
+                    product
+                )
+            }
         )
     }
 
--- app/src/main/java/com/example/produra/presentation/productList/components/ProductCard.kt
+++ app/src/main/java/com/example/produra/presentation/productList/components/ProductCard.kt
@@ -2,8 +2,10 @@ package com.example.produra.presentation.productList.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,6 +13,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,30 +28,46 @@ import com.example.produra.model.Product
 fun ProductCard(
     modifier: Modifier = Modifier, product: Product, toggleProductMustBePurchased: (Product) -> Unit
 ) {
+    var showDetails by remember { mutableStateOf(false) }
+
     val alignment = Alignment.CenterVertically
-    Row(
-        modifier
-            .background(Color.White)
-            .fillMaxWidth()
-            .border(2.dp, MaterialTheme.colorScheme.secondary),
-        verticalAlignment = alignment,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Box(Modifier.padding(15.dp, 15.dp, 15.dp, 15.dp)) {
-            Text(text = product.name)
-        }
-        Box {
-            Row(Modifier.padding(15.dp, 15.dp, 15.dp, 15.dp), verticalAlignment = alignment) {
-                println(product.id.toString() + " " + product.mustBePurchased.toString())
-                Button(onClick = { toggleProductMustBePurchased(product) }) {
-                    if (product.mustBePurchased) {
-                        Text(text = "Necesito comprar este producto")
-                    } else {
-                        Text(text = "Ya compré este producto")
+    Column {
+        Row(
+            modifier
+                .background(Color.White)
+                .fillMaxWidth()
+                .border(2.dp, MaterialTheme.colorScheme.secondary),
+            verticalAlignment = alignment,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box(
+                Modifier
+                    .padding(15.dp, 15.dp, 15.dp, 15.dp)
+                    .clickable { showDetails = !showDetails }) {
+                Text(text = product.name)
+            }
+            Box {
+                Row(Modifier.padding(15.dp, 15.dp, 15.dp, 15.dp), verticalAlignment = alignment) {
+                    Button(onClick = { toggleProductMustBePurchased(product) }) {
+                        if (product.mustBePurchased) {
+                            Text(text = "Necesito comprar este producto")
+                        } else {
+                            Text(text = "Ya compré este producto")
+                        }
                     }
                 }
             }
         }
+        if (showDetails) {
+            Details(product = product)
+        }
+    }
+}
+
+@Composable
+fun Details(product: Product) {
+    Box {
+        Text(text = product.description)
     }
 }
 
